454358b5773a0d735b4e8f7d1b5eb7efa249c76e
Mariana Barreiros <maridepp1@hotmail.com>
2018-07-07 12:22:26 -0300

    display block e variaveis

--- variaveis-personagem-secundario.js
+++ variaveis-personagem-secundario.js
@@ -10,7 +10,7 @@ let mostra_o_secundario = $('#card-jogador-secundario');
  vai adicionar ao time do seu colega, altere
  o valor da variável abaixo para 'display', 'block'.
 */
-mostra_o_secundario.css('display', 'none');
+mostra_o_secundario.css('display', 'block');
 
 /*
  Aqui foram definidas as variáveis do personagem secundário. Não precisa mexer aqui. :)
@@ -31,14 +31,14 @@ let magica_personagem_2 = $('#magica_personagem_2');
  Aqui foram definidos os valores iniciais para os atributos do personagem secundário.
  Altere os dados do pesonagem modificando os valores das variáveis abaixo!
 */
-tipo_personagem2_1.html('Elfo');
-tipo_personagem2_2.html('Venenoso');
+tipo_personagem2_1.html('Vampiro');
+tipo_personagem2_2.html('Humano Assassino');
 tipo_personagem2_3.html('Fantasma');
-tipo_personagem2_4.html('Sombrio');
+tipo_personagem2_4.html('Zombie');
 
-forca_personagem_2.css('width', '20%');
+forca_personagem_2.css('width', '30%');
 carisma_personagem_2.css('width', '20%');
-sabedoria_personagem_2.css('width', '30%');
+sabedoria_personagem_2.css('width', '10%');
 velocidade_personagem_2.css('width', '40%');
-regeneracao_personagem_2.css('width', '50%');
-magica_personagem_2.css('width', '100%');
+regeneracao_personagem_2.css('width', '20%');
+magica_personagem_2.css('width', '10%');
